3428d382c287fb2b4c9dad71fabcfce455c88021
Stevedeanda <stevedeanda5@gmail.com>
2023-03-23 13:31:40 -0500

    Configuring and getting files in place

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslint from '@rollup/plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+   plugins: [
+      vue(),
+      {
+         ...eslint(),
+         enforce: 'pre',
+         apply: 'build',
+      },
+   ],
+   resolve: {
+      alias: {
+         '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+   },
+});
