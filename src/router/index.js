214b8f674035d37f2a8c92f9d34255e9e29a021d
Stevedeanda <stevedeanda5@gmail.com>
2023-03-28 15:01:28 -0500

    Add the contactview routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-import MoreView from '../views/MoreView.vue';
+import ContactView from '../views/ContactView.vue';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,9 +19,9 @@ const router = createRouter({
 			component: () => import('../views/AboutView.vue'),
 		},
 		{
-			path: '/more',
-			name: 'more',
-			component: MoreView,
+			path: '/contact',
+			name: 'contact',
+			component: ContactView,
 		},
 	],
 });
