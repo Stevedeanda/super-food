964cd8a4c2e847cc1db2b2d7943f329ffb2c1f59
Stevedeanda <stevedeanda5@gmail.com>
2023-03-28 14:59:30 -0500

    Added contactview

---
+++ src/components/MonsterForm.vue
@@ -0,0 +1,26 @@
+<script setup>
+	import { reactive } from 'vue';
+
+	const monster = reactive({
+		name: 'Steve',
+		age: 33,
+	});
+
+	function saved() {
+		alert('save');
+	}
+</script>
+
+<template>
+	<form @submit.prevent="saved()">
+		<div class="field">
+			<label for="w">Monster Name</label>
+			<input id="w" type="text" v-model="monster.name" />
+		</div>
+		<div class="field">
+			<label for="h">Age</label>
+			<input id="h" type="number" v-model="monster.age" />
+		</div>
+		<button type="submit">Submit</button>
+	</form>
+</template>
