83cbc8ef57ef0db802f66041f03aa17506f08c4a
Stevedeanda <stevedeanda5@gmail.com>
2023-03-29 21:35:40 -0500

    Deleted the placeholder

--- src/stores/monsters.js
+++ src/stores/monsters.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 
 export const useMonsterStore = defineStore('monsters', function () {
 	// monster list
-	const list = reactive([{ name: 'one' }, { name: 'two' }]);
+	const list = reactive([]);
 	// add a monster
 	function add(monster) {
 		list.push(monster);
